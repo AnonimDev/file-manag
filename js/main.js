ad82608cbb286105b5d9da5b1b5d4d44392c9af1
AnonimDev <anonims220@gmail.ru>
2018-02-05 22:24:43 +0300

    Добавил возможность отключения анимации

--- js/main.js
+++ js/main.js
@@ -1,94 +1,73 @@
 $(document).ready( function () {
+    var anim = true;
+    ajaxOpenDir();
 
-    q();
     $('.box').on('click', 'input', function(){
         var dir = $(this).val();
-        q(dir);
+        ajaxOpenDir(dir);
     });
+
     $('.box').on('click', 'label', function(){
 
         var dir = $(this).prev();
         dir = $(dir).val();
-        q(dir);
+        ajaxOpenDir(dir);
     });
 
     $(document).on('change', '.sel', function () {
-        b();
+        fontSizeAnim(anim);
+    });
+    $(document).on('change', '.anim', function () {
+        var animElem = $('.anim').val();
+        anim = (animElem == 0) ? false : true;
     });
 
-    function q(a) {
-        if (a == undefined){
-            var a = '';
+    function ajaxOpenDir(dir) {
+        if (dir == undefined){
+            var dir = '';
         }
         $.ajax({
             type: "POST",
             url: 'php/obr.php',
             data: {
-                dir: a
+                dir: dir
             },
             success: function(data){
+
                 $('.box').text('');
                 $('.box').append(data);
-                b(1);
-                $('.box-elem').slideDown(800);
-
+                fontSizeAnim(0);
+                if(anim == 0) {
+                    $('.box-elem').show();
+                } else $('.box-elem').slideDown(800);
             }
         });
     }
-    function b(a) {
+    function fontSizeAnim(animat) {
         var sel = $('.sel').val();
-        if (a == undefined) {
+        if (animat == 1) {
             if (sel == 1) {
-                $(".box").animate({
-                    fontSize: '15px'
-                });
-                $(".elem, .elem-info, .elem-size").animate({
-                    height: '20px'
-                });
+                $(".box").animate({ fontSize: '15px' });
+                $(".elem, .elem-info, .elem-size").animate({ height: '20px' });
             } else if (sel == 2) {
-                $(".box").animate({
-                    fontSize: '25px'
-                });
-                $(".elem, .elem-info, .elem-size").animate({
-                    height: '30px'
-                });
+                $(".box").animate({ fontSize: '25px' });
+                $(".elem, .elem-info, .elem-size").animate({ height: '30px' });
             } else if (sel == 3) {
-                $(".box").animate({
-                    fontSize: '35px'
-                });
-                $(".elem").animate({
-                    width: '30% !important'
-                });
-                $(".elem, .elem-info, .elem-size").animate({
-                    height: '45px'
-                });
+                $(".box").animate({ fontSize: '35px' });
+                $(".elem").animate({ width: '30% !important' });
+                $(".elem, .elem-info, .elem-size").animate({ height: '45px' });
             }
-        }
-        else {
+        } else if(animat == 0) {
             if (sel == 1) {
-                $(".box").css(
-                    'fontSize', '15px'
-                );
-                $(".elem, .elem-info, .elem-size").css(
-                    'height', '20px'
-                );
+                $(".box").css( 'fontSize', '15px' );
+                $(".elem, .elem-info, .elem-size").css( 'height', '20px' );
             } else if (sel == 2) {
-                $(".box").css(
-                    'fontSize', '25px'
-                );
-                $(".elem, .elem-info, .elem-size").css(
-                    'height', '30px'
-                );
+                $(".box").css( 'fontSize', '25px' );
+                $(".elem, .elem-info, .elem-size").css( 'height', '30px' );
             } else if (sel == 3) {
-                $(".box").css(
-                    'fontSize', '35px'
-                );
-                $(".elem").css(
-                    'width', '30% !important'
-                );
-                $(".elem, .elem-info, .elem-size").css(
-                    'height', '45px'
-                );
+                $(".box").css( 'fontSize', '35px' );
+                $(".elem").css( 'width', '30% !important' );
+                $(".elem, .elem-info, .elem-size").css( 'height', '45px' );
             }
         }
     }

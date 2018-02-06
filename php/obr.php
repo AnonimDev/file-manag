55dd07168937782bf4269ab301f6f5fd541a45e0
Аноним <AnonimDev>
2018-02-06 23:42:02 +0300

    Исправил баг с преведущей директорией

--- php/obr.php
+++ php/obr.php
@@ -2,25 +2,26 @@
 
 if (isset($_POST['dir'])){
     $dir = $_POST['dir'];
+
     if (empty($dir)) {
-        $dir = not_repeat(dirname(__FILE__));
-    } else not_repeat($dir);
-        echo open($dir);
+        $dir = kor();
+    }
+
+    echo open($dir);
 } else echo 'Ошибка при отправке данных!!!';
 
+function kor(){
+    $arr = explode(DIRECTORY_SEPARATOR, dirname(__FILE__));
+    $arr = array_shift($arr);
+    //$arr .= DIRECTORY_SEPARATOR;
+    return $arr;
+}
 
 function not_repeat($dir){
-
     $arr = explode(DIRECTORY_SEPARATOR, $dir);
-    $arr = array_shift($arr);
-    $arr .= DIRECTORY_SEPARATOR;
-
-    if ($dir = $arr) {
-        $arr = explode(DIRECTORY_SEPARATOR, $dir);
-        array_pop($arr);
-        $arr = implode(DIRECTORY_SEPARATOR, $arr);
-        return $arr;
-    } else return $dir;
+    array_pop($arr);
+    $arr = implode(DIRECTORY_SEPARATOR, $arr);
+    return $arr;
 }
 
 function filesize_format($filesize){
@@ -37,31 +38,41 @@ function filesize_format($filesize){
     return $filesize.$formats[$format];
 }
 
-function open($dir){
-
+function opDir($dir){
     $dirs = array();
     $files = array();
-    $rez = '';
-
     $path = trim($dir, '/');
-    $rez .= '<label>Текущий путь:&nbsp;&nbsp;&nbsp;'.$path.'</label>';
-    if ($dir = @opendir($path)) {
-        while (false !== ($file = readdir($dir))) {
-            if ($file == '.' || $file == '..') continue;
+    if (!is_file($dir)) {
+        if ($dir = @opendir($path)) {
+            while (false !== ($file = readdir($dir))) {
+                if ($file == '.' || $file == '..') continue;
 
-            if (is_file($path . DIRECTORY_SEPARATOR . $file)) {
-                $files[] = $path . DIRECTORY_SEPARATOR . $file;
-            } else {
-                $dirs[] = $path . DIRECTORY_SEPARATOR . $file;
+                if (is_file($path . DIRECTORY_SEPARATOR . $file)) {
+                    $files[] = $path . DIRECTORY_SEPARATOR . $file;
+                } else {
+                    $dirs[] = $path . DIRECTORY_SEPARATOR . $file;
+                }
+                sort($dirs);
+                sort($files);
             }
-            sort($dirs);
-            sort($files);
-        }
 
-        closedir($dir);
+            closedir($dir);
+            $rezult[] = $dirs;
+            $rezult[] = $files;
+            return $rezult;
+        } else return 101;
+    } else return 100;
+}
 
+function open($dir){
+    $rez = '';
+    $rezult = opDir($dir);
+    $rez .= '<label>Текущий путь:&nbsp;&nbsp;&nbsp;'.$dir.'</label>';
 
-        $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' . $arr . '"/>&nbsp;<label>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></p></div><div class="elem-size"></div><div class="elem-info"></div></div>';
+    if (!empty($rezult) and $rezult != 100 and $rezult != 101){
+        $dirs = $rezult[0];
+        $files = $rezult[1];
+        $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' . not_repeat($dir) . '"/>&nbsp;<label>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></p></div><div class="elem-size"></div><div class="elem-info"></div></div>';
         if (!empty($dirs)) {
             foreach ($dirs as $key => $value) {
                 $a = mb_strtolower(mb_substr(mb_strrchr($dirs[$key], DIRECTORY_SEPARATOR), 1));
@@ -71,11 +82,14 @@ function open($dir){
         if (!empty($files)) {
             foreach ($files as $key => $value) {
                 $a = mb_strtolower(mb_substr(mb_strrchr($files[$key], DIRECTORY_SEPARATOR), 1));
-                $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' . $files[$key] . '"/>&nbsp;<label>' . $a . '</label></p></div><div class="elem-size">' . filesize_format(filesize($files[$key])) . '</div><div class="elem-info">Файл</div></div>';
+                $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' .  $files[$key] . '"/>&nbsp;<label>' . $a . '</label></p></div><div class="elem-size">' . filesize_format(filesize($files[$key])) . '</div><div class="elem-info">Файл</div></div>';
             }
         }
+    } else if($rezult === 100) {
+        $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' .  not_repeat($dir) . '"/>&nbsp;<label>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></p></div><div class="elem-size"></div><div class="elem-info"></div></div>';
+        $rez .= "<br><br>Такие файлы я пока не умею открывать ='(";
     } else {
-        $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' . $arr . '"/>&nbsp;<label>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></p></div><div class="elem-size"></div><div class="elem-info"></div></div>';
+        $rez .= '<div class="box-elem"><div class="elem"><p><input id="checkbox" type="checkbox" value="' .  not_repeat($dir) . '"/>&nbsp;<label>/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></p></div><div class="elem-size"></div><div class="elem-info"></div></div>';
         $rez .= '<br><br>Не могу открыть директорию';
     }
 
